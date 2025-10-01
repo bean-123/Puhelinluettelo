e317a010adccc61d5d01b1f39d912f2b5a466155
bean-123 <amy.platt@hotmail.com>
2025-10-01 11:25:24 +0300

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
   const [inputText, setInputText] = useState("");
