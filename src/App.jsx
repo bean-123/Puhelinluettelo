81963a69e0d7ef109807d0195e195bb6bd614c7d
bean-123 <amy.platt@hotmail.com>
2025-10-01 11:30:13 +0300

    Add Names component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Button from "./components/Button";
 import Header from "./components/Header";
 import Input from "./components/Input";
+import Names from "./components/Names";
 
 const App = () => {
   const [inputText, setInputText] = useState("");
---
+++ src/components/Names.jsx
@@ -0,0 +1,9 @@
+const Names = (props) => {
+  return (
+    <>
+      {props.name} {props.number}
+    </>
+  );
+};
+
+export default Names;
